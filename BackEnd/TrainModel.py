cc494bd7cce4dea23459d8b41e793be367072832
Giovanna Dias <gidiasdossantos@hotmail.com>
2026-06-03 22:02:13 -0300

    Create TrainModel.py

---
+++ BackEnd/TrainModel.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import pickle
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+dados = pd.read_csv("../dataset/spam.csv")
+
+X = dados["mensagem"]
+y = dados["classe"]
+
+vectorizer = TfidfVectorizer()
+
+X_tfidf = vectorizer.fit_transform(X)
+
+modelo = MultinomialNB()
+modelo.fit(X_tfidf, y)
+
+pickle.dump(modelo, open("model/spam_model.pkl", "wb"))
+pickle.dump(vectorizer, open("model/vectorizer.pkl", "wb"))
+
+print("Modelo treinado com sucesso!")
